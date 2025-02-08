80d6c36199cbe26470189b93d0aa2926b286a13e
Argus <argushakizimana@gmail.com>
2025-02-08 18:25:55 +0200

    fixed build error

--- packages/react-app/hooks/usePayment.ts
+++ packages/react-app/hooks/usePayment.ts
@@ -104,7 +104,7 @@ import erc20Abi from "../utils/erc20Abi.json";
 import cusdAbi from "../utils/cusdAbi.json";
 import { publicClient } from "../utils/publicClient"; 
 
-export const usePayments = () => {
+export const usePayments = (walletAddress: string | null) => {
   const [loading, setLoading] = useState(false);
   const taxiPaymentContractAddress = "0x7f8EFB57b228798d2d3ec3339cD0a155EB3B0f96";
   const cusdTokenAddress = "0x765de816845861e75a25fca122bb6898b8b1282a";
