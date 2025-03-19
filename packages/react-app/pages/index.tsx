eac7d1d7b0f28208ac305b8ec7ee126f80862505
Argus <argushakizimana@gmail.com>
2025-03-19 16:43:15 +0200

    service worker updates for pwa

--- packages/react-app/pages/index.tsx
+++ packages/react-app/pages/index.tsx
@@ -2,6 +2,20 @@ import LandingPage from "./landingpage";
 
 
 export default function Home() {
+
+  if ("serviceWorker" in navigator) {
+
+    window.addEventListener("load", function () {
+      navigator.serviceWorker.register("/service-worker.js").then(
+        function (registration) {
+          console.log("Service Worker registration successful with scope: ", registration.scope);
+        },
+        function (err) {
+          console.log("Service Worker registration failed: ", err);
+        }
+      );
+    });
+  }
   return  (
     <>
       <LandingPage />
